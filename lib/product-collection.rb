b639165791f4b32e28e851650f4d87c98c3a702b
Lemeri02 <9588504@gmail.com>
2019-11-27 21:50:35 +0500

    product collection

--- lib/product-collection.rb
+++ lib/product-collection.rb
@@ -1,27 +1,45 @@
-class ProductCollection < Product
-  def self.from_dir(file_path)
-    @movies = file_path[:movies]
-    @books = file_path[:books]
- 
-    @movies.map do |file|
-      lines = File.readlines(
-        file,
-        encoding: 'UTF-8',
-        chomp: true
-      ).map(&:chomp)
-      
+class ProductCollection
+  PRODUCT_TYPES = {
+    film: { dir: 'films', class: Movie },
+    book: { dir: 'books', class: Book }
+  }
+
+  def initialize(products = [])
+    @products = products
+  end
+
+  def self.from_dir(dir_path)
+    products = []
+
+    PRODUCT_TYPES.each do |type, hash|
+      product_dir = hash[:dir]
+
+      product_class = hash[:class]
+
+      Dir[dir_path + '/' + product_dir + '/*.txt'].each do |path|
+        products << product_class.from_file(path)
+      end
     end
+
+    self.new(products)
   end
-  
-  def initialize
-    
-  end
-  
-  def sort
-      
+
+  def sort!(params)
+    case params[:by]
+    when :title
+      @products.sort_by!(&:to_s)
+    when :price
+      @products.sort_by!(&:price)
+    when :amount
+      @products.sort_by!(&:amount)
+    end
+
+    @products.reverse! if params[:order] == :asc
+
+    self
   end
 
   def to_a
-    @movies + @books
+    @products
   end
 end
--- main.rb
+++ main.rb
@@ -2,28 +2,10 @@ require_relative 'lib/product'
 require_relative 'lib/movie'
 require_relative 'lib/book'
 require_relative 'lib/product-collection'
+ 
+collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
+collection.sort!(by: :price, order: :asc)
 
-
-
-products = [
-  Book.new(title: "Идиот", genre: "роман", author: "Федер Достоевский", price: 1500, amount: 10),
-  Movie.new(title: "Леон", year: 1994, director: "Люк Бессон", price: 900, amount: 5),
-  Movie.new(title: "Дурак", year: 2014, director: "Юрий Быков", price: 390, amount: 1)
-]
-
-puts products.each(&:to_s)
-
-path = {
-  books: Dir["#{__dir__}/data/books/*.txt"],
-  movies: Dir["#{__dir__}/data/films/*.txt"]
-}
-
-# movie = Movie.from_file("#{__dir__}/data/films/01.txt")
-# book = Book.from_file("#{__dir__}/data/books/01.txt")
-colect = ProductCollection.from_dir(path)
-
-m = ProductCollection.new
-# puts movie
-# puts book
-# puts "main.rb:27 #{colect}"
-puts m.to_a
+collection.to_a.each do |product|
+  puts product
+end
