3e315e1ab1a704e296eb28f4c4fc9cba5354459b
Lemeri02 <9588504@gmail.com>
2019-11-28 23:24:23 +0500

    create cart

--- main.rb
+++ main.rb
@@ -7,12 +7,9 @@ require_relative 'lib/product-collection'
 collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
 collection.sort!(by: :price, order: :asc)
 
-# collection.to_a.each do |product|
-#   puts product
-# end
-
-choice = -1
+choice = 50
 shopping_cart = []
+result = 0
 
 until choice == 0
   puts 'Что хотите купить:'
@@ -20,15 +17,32 @@ until choice == 0
     puts %(#{index}. #{product})
   end
   puts '0. Выход'
-
+ 
   choice = STDIN.gets.to_i
-
-  shopping_cart << collection.to_a[choice - 1] if choice != 0
+  chosen_product = collection.to_a[choice - 1]
+ 
+  unless choice <= 0
+    if chosen_product.amount > 0
+      chosen_product.update(amount: (chosen_product.amount - 1))
+      shopping_cart << chosen_product
+      puts %(Вы выбрали: #{chosen_product})
+      puts %(Всего товаров на сумму: #{chosen_product.price} руб.)
+    else
+      puts
+      puts 'Такого товара больше нет. Выберите другой :)'
+      puts
+    end
+  end
 end
 
+shopping_cart.each do |product|
+  result += product.price
+end
 
-result = 0
-#puts collection.to_a[0].price
-shopping_cart.map{ |price| result += price.price }
-puts result
-p shopping_cart.to_a
+if result != 0
+  puts 'Вы купили:'
+  puts shopping_cart
+  puts "С вас - #{result} руб. Спасибо за покупки!"
+else
+  puts 'Приходите к нам снова!'
+end
